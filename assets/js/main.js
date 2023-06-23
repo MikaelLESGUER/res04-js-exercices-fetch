2a073b3534e485cf1c8cd804e5e68dee98d5c31b
MikaëlLESGUER <mikedev35@outlook.fr>
2023-06-23 09:05:33 +0000

    mise en place

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// let url = "https://jsonplaceholder.typicode.com/users"
+
+// fetch(url)
+// .then(function(response) {
+//   return response.json();
+// })
+// .then(function(result) {
+//   console.log(result);
+// });
+
+import {api} from './data/data.js'
+
+import { Library } from './library.js';
